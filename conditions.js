50acff1037a18f6d62473d0ef32c0f30bba98953
Ammad <syedammad0.46@gmail.com>
2022-03-18 19:43:19 +0500

    Variable And Condition ADD

---
+++ conditions.js
@@ -0,0 +1,55 @@
+// Conditions =====>
+// condition Statement MUST TAKE VALUE in BOLEANS === true/false
+
+// IF ELSE
+if (false) {
+    console.log("TRUE");
+  } else {
+    console.log("FALSE");
+  }
+  
+  // if ELSE CHAINING ===> if compiler gets any condition true .. (IT STOP EXECUTION) == NEVER GO FORWARD
+  
+  if (false) {
+    console.log("A");
+  } else if (true) {
+    console.log("B");
+  } else {
+    console.log("C");
+  }
+  
+  // Logical OPERARORS ====> (!) .. (&&) ... (||)
+  
+  // TRUE : IF ALL CONDITIONS ARE TRUTHY
+  if(true && true && true){
+      console.log('TRUEXXXX');
+  }
+  
+  // True : if Single Condition Is True
+  if(true || false || false){
+      console.log('TRUEXXXX');
+  }
+  
+  
+  let x = false
+  // ! : Reverse
+  if(!x){
+  console.log('TRUTHY');
+  }
+  
+  
+  
+  // students == marks === 50 > D , 60 > C , 70 > B , 80 > A
+  
+  const marks = 60;
+  
+  if (marks >= 50){
+      console.log('D');
+  }else if(marks >= 60){
+      console.log('C');
+  }else if(marks >= 70){
+      console.log('B');
+  }else{
+      console.log('A');
+  }
+  
---
+++ variables.js
@@ -0,0 +1,26 @@
+// Varibals
+
+// Primitive ====> Non-Primitive
+
+// Primitive
+// numbers,String,boolean,undefined,null
+
+// NoN-Primitive
+// Array,Objects,Set,Hash
+
+// var , let , const  // Syntax ====>  let varibaleName = value
+
+// DATA_TYPES ===>  Numbers,String,booleans
+let name = "USAMA";
+let age = 1000;
+let isMarried = true;
+
+// VariableName =======
+let x = 10;
+x = 20;
+
+// constants ===> Fixed Values
+const y = 10;
+y = 20;
+
+console.log(x);
